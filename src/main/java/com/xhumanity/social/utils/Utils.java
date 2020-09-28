e02694894a48eff87d2abc1f9df7bb4eb90db292
iulian <iuliancl2000@yahoo.com>
2020-09-29 00:42:04 +0300

    get rid of StringUtils

--- src/main/java/com/xhumanity/social/utils/Utils.java
+++ src/main/java/com/xhumanity/social/utils/Utils.java
@@ -3,14 +3,12 @@ package com.xhumanity.social.utils;
 import java.text.SimpleDateFormat;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class Utils {
 
 	public static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	
 	public static boolean isEmailValid(String email) {
-		if (StringUtils.isBlank(email)) {
+		if (email == null) {
 			return false;
 		} else {
 			String regex = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
