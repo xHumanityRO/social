e69345478b1663ded6805f01e08f405347bd6f9c
Iulian Cristian Lungu <Iulian_Cristian.Lungu@lidl.ro>
2020-10-14 15:15:48 +0300

    fix get reputation sql

--- src/main/java/com/xhumanity/social/repository/InstagramInsightRepository.java
+++ src/main/java/com/xhumanity/social/repository/InstagramInsightRepository.java
@@ -14,14 +14,14 @@ public interface InstagramInsightRepository  extends JpaRepository<InstagramInsi
 
 	Optional<InstagramInsight> findByMediaUrl(@Param("mediaUrl") String mediaUrl);
 
-	@Query(value = "select DATE(creation_time) as day, sum(a.likes - a.dislikes + (\n" + 
+	@Query(value = "select DATE(a.creation_time) as day, sum(a.likes - a.dislikes + (\n" + 
 			"			select count(*) from instagram_insight_comment b where b.insight_id = a.id)) as reputation \n" + 
 			"	from instagram_insight a \n" + 
 			"	  inner join campaign_video c on a.media_url = c.link\n" + 
 			"	  inner join telegram_users t on c.user_id = t.id AND t.forum_user_id = ?1\n" + 
-			"	where a.id in (select max(ii.id) from instagram_insight ii group by media_url, DATE(creation_time))\n" + 
-			"	group by DATE(creation_time)\n" + 
-			"	order by DATE(creation_time)", nativeQuery = true)
+			"	where a.id in (select max(ii.id) from instagram_insight ii group by media_url, DATE(ii.creation_time))\n" + 
+			"	group by DATE(a.creation_time)\n" + 
+			"	order by DATE(a.creation_time)", nativeQuery = true)
     List<ReputationProjection> getReputation(Integer forumUserId);
 
 }
